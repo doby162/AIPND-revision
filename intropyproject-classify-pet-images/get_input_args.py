29a9ea2214e06fb7aba28bed3f0e281675b559d2
Michael Dorian <doby162@gmail.com>
2020-02-08 15:12:52 -0600

    parse command args

--- intropyproject-classify-pet-images/get_input_args.py
+++ intropyproject-classify-pet-images/get_input_args.py
@@ -40,4 +40,26 @@ def get_input_args():
     """
     # Replace None with parser.parse_args() parsed argument collection that
     # you created with this function
-    return None
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--dir",
+        type=str,
+        default="pet_images/",
+        help="path to the folder of pet images",
+    )
+
+    parser.add_argument(
+        "--arch", type=str, default="resnet", help="specify CNN model to use",
+    )
+
+    parser.add_argument(
+        "--dogfile",
+        type=str,
+        default="dognames.txt",
+        help="path to the file with a list of valid dog names",
+    )
+
+    in_args = parser.parse_args()
+    return in_args
+    # print("Argument 1:", in_args.dir)
